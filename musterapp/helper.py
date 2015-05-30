425716db64081d437408687e5885014a7b7a7a87
jo <kleingeist@googlemail.com>
2015-05-30 11:45:45 +0200

    Add helper functions for record ids

---
+++ musterapp/helper.py
@@ -0,0 +1,47 @@
+import re
+
+_recid_volume = "HA.II.{:02d}"
+_recid_single = "HA.II.{:02d}.{:03d}"
+_recid_double = "HA.II.{:02d}.{:03d}-{:03d}"
+_recpattern = re.compile(
+    "^HA\.II\. (\d{2})  (?:\.(\d{3}))?  (?:-(\d{3}))? $", re.X)
+_imgname = "Htw-berlin-stoffmuster-ha02-{:02d}-{:03d}.jpg"
+_imgname_double = "Htw-berlin-stoffmuster-ha02-{:02d}-{:03d}-{:03d}.jpg"
+_imgpattern = re.compile(
+    "^Htw-berlin-stoffmuster-ha02- (\d{2}) -(\d{3}) (?:-(\d{3}))? .jpg$", re.X)
+
+
+def split_recid(recid):
+    match = _recpattern.match(recid)
+    if match is None:
+        raise ValueError("Invalid record id pattern: {}".format(recid))
+
+    volid = int(match.group(1))
+    page1 = int(match.group(2)) if match.group(2) else None
+    page2 = int(match.group(3)) if match.group(3) else None
+
+    return volid, page1, page2
+
+def recid2img(recid):
+    volid, page1, page2 = split_recid(recid)
+    if page1 is None:
+        raise ValueError("Invalid record id, page missing: {}".format(recid))
+
+    _recid = _imgname if page2 is None else _imgname_double
+    return _recid.format(volid, page1, page2)
+
+def img2recids(imgname):
+    match = _imgpattern.match(imgname)
+    if match is None:
+        raise ValueError("Invalid image name pattern: {}".format(imgname))
+
+    volid = int(match.group(1))
+    page1 = int(match.group(2))
+    page2 = int(match.group(3)) if match.group(3) else None
+
+    recid = _recid_single if page2 is None else _recid_double
+
+    return (_recid_volume.format(volid),
+            recid.format(volid, page1, page2),
+            _recid_single.format(volid, page1) if page2 is not None else None,
+            _recid_single.format(volid, page2) if page2 is not None else None)
--- musterapp/tests.py
+++ musterapp/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import helper
+
+
+
+class HelperTests(TestCase):
+
+    def test_recid2img(self):
+        self.assertEqual(
+            "Htw-berlin-stoffmuster-ha02-02-001.jpg",
+            helper.recid2img("HA.II.02.001")
+        )
+
+        self.assertEqual(
+            "Htw-berlin-stoffmuster-ha02-02-001-002.jpg",
+            helper.recid2img("HA.II.02.001-002")
+        )
+
+        with self.assertRaises(ValueError):
+            helper.recid2img("HA.II.02")
+
+        with self.assertRaises(ValueError):
+            helper.recid2img("HA.II.02.02")
+
+    def test_img2recids(self):
+        vol, page, page1, page2 = helper.img2recids(
+            "Htw-berlin-stoffmuster-ha02-02-001.jpg")
+        self.assertEqual(vol, "HA.II.02")
+        self.assertEqual(page, "HA.II.02.001")
+        self.assertEqual(page1, None)
+        self.assertEqual(page2, None)
+
+        vol, page, page1, page2 = helper.img2recids(
+            "Htw-berlin-stoffmuster-ha02-02-001-002.jpg")
+        self.assertEqual(vol, "HA.II.02")
+        self.assertEqual(page, "HA.II.02.001-002")
+        self.assertEqual(page1, "HA.II.02.001")
+        self.assertEqual(page2, "HA.II.02.002")
+
+        with self.assertRaises(ValueError):
+            helper.img2recids("Htw-berlin-stoffmuster-ha02-02-01.jpg")
